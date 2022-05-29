ee32bc8ab870d49ce6665a924a50054926bae8e9
ZKS12 <105759837+ZKS12@users.noreply.github.com>
2022-05-29 17:52:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,11 +11,11 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- This bot was created for Zerox movie club. 
+- This bot was created for Zerox movie club🍀. 
 - channel link -   https://t.me/+lKtowsIlFWtjZTA9
 
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+<b>link to contact owner:</b>
+- <a href=https://t.me/ZEROX_AS>ZEROX❤️</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
